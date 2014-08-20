db95ce4125e2868fff0fde834c4f23f2213d5c6b
santiago <santiago.ruiz.navas@gmail.com>
2014-08-20 11:48:28 +0900

    Coursera 2nd assigment

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,33 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# the matriz i receive is square, inversible and valid matrix.
 makeCacheMatrix <- function(x = matrix()) {
-
+    i <- NULL #mean saver variable
+    set <- function(y) { #Will get the expected matrix
+        x <<- y
+        i <<- NULL # reseting the mean value and the new matrix
+    }
+    get <- function() x #gets the value of the matrix
+    setinv <- function(inv) i <<- inv# i dont know how it calculates if i dont give the function anything to operate
+    getinv <- function() i #retrieves the calculated value
+    list(set = set, get = get, #sets the output list. 
+         setinv = setinv,
+         getinv = getinv)
 }
 
 
 ## Write a short comment describing this function
-
+#This one recieve the super matrix created above
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    i <- x$getinv()
+    if(!is.null(i)) {
+        message("getting cached data")
+        return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinv(i)
+    i
 }
